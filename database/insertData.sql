0962a05a9c9f92f82eccf2441c1d8ec46107e746
marcos <marcos.azcarate@alumni.mondragon.edu>
2018-12-17 10:59:58 +0100

    DataBase user create

---
+++ database/insertData.sql
@@ -0,0 +1,2 @@
+INSERT INTO users values (1,'Marcos','Azcarate','Hulio14','marcos.azcarate@alumni.mondragon.edu','osasuna');
+INSERT INTO warehouseOperator values (1,'Jon','Mugica','ScrumBanBoy','123456789',true);
